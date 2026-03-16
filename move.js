3b240694c0d8ac13c8bc6ef42adf8f17514460ef
ikdknt2 <ikdknt2@gmail.com>
2026-03-16 17:14:05 +0900

    Update move.js

--- move.js
+++ move.js
@@ -45,5 +45,15 @@ function hedge(){
   draw(state)
 }
 
-function y(){
- do4cycle()
+function up(){
+ do4cycle(6,7,8,9)
+ do4cycle(2,23,26,11)
+ do4cycle(3,24,27,12)
+ do4cycle(0,20,25,10)
+ do4cycle(1,21,29,14)
+ do4cycle(4,24,28,13)
+ do4cycle(16,17,18,19)
+
+ draw(state)
+}
+
